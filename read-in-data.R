e6cbd8c899e0f2393bb736a73c8a87d28324ab4a
eugenividal <eugenividal@hotmail.com>
2022-10-14 18:36:10 +0100

    update

---
+++ read-in-data.R
@@ -0,0 +1,4 @@
+# Read the data
+destino <- read.csv("datos_disponibles/DIA_01ABR_2020/PobxCeldasDestinoM1_01ABR.csv")
+origen <- read.csv("datos_disponibles/DIA_01ABR_2020/PobxCeldasOrigenM1_01ABR.csv")
+flujos <- read.csv("datos_disponibles/DIA_01ABR_2020/Flujos+15_O-D_M1_01ABR.csv")
