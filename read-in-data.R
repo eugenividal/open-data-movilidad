a0428092b56e1967ccfc5493dee4baa9408fc36b
eugenividal <eugenividal@hotmail.com>
2022-10-31 16:08:32 +0000

    update

--- read-in-data.R
+++ read-in-data.R
@@ -1,32 +1,14 @@
 # Read the data
-# getwd()
-# 
-# setwd("/home/eugeni/Documents/INE-estudio-mobilidad-datos/")
-
-data = read.table(gzfile("202104_maestra1_mitma_distrito/20210401_maestra_1_mitma_distrito.txt.gz"), header = TRUE, sep = "|")
-
-municipios = read.table("relaciones_municipio_mitma.csv", header = TRUE, sep = "|")
-
-distritos = read.table("relaciones_distrito_mitma.csv", header = TRUE, sep = "|")
+data = read.table(gzfile("datos/202104_maestra1_mitma_distrito/20210401_maestra_1_mitma_distrito.txt.gz"), header = TRUE, sep = "|")
 
 names(data)
 
+municipios = read.table("datos/relaciones_municipio_mitma.csv", header = TRUE, sep = "|")
+distritos = read.table("datos/relaciones_distrito_mitma.csv", header = TRUE, sep = "|")
+
 zonificacion_distritos <- sf::st_read(
-  "zonificacion_distritos/zonificacion-distritos/distritos_mitma.shp")
+  "datos/zonificacion_distritos/zonificacion-distritos/distritos_mitma.shp")
 
 zonificacion_municipios <- sf::st_read(
-  "zonificacion_municipios/zonificacion-municipios/municipios_mitma.shp")
+  "datos/zonificacion_municipios/zonificacion-municipios/municipios_mitma.shp")
 
-install.packages("XML")
-
-# Load the package required to read XML files.
-library("XML")
-
-# Also load the other required package.
-library("methods")
-
-# Give the input file name to the function.
-result <- xmlParse(file = "RSS.xml")
-
-# Print the result.
-print(result)
